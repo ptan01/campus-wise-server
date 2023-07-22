47a93002f23f59f5e36ce541993d991fa868498b
ptan01 <ptanjim01@gmail.com>
2023-07-22 17:15:59 +0600

    using dotenv and create all collage api

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000 ;
 const cors = require("cors")
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(cors())
 app.use(express.json())
 
-// user: campusWise
-// pass : 0V0ZZjNdx7PcL2Mc
 
 
 
-
-const uri = "mongodb+srv://campusWise:0V0ZZjNdx7PcL2Mc@cluster0.8hd0j1r.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8hd0j1r.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +26,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collageCollection = client.db('campusDB').collection('collage') ;
+
+    app.get('/collage',async(req, res)=> {
+        const result = await collageCollection.find().toArray() ;
+        res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
