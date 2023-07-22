fb1484bdd135e29b8a79d7c3b26b6d7f6170aa63
ptan01 <ptanjim01@gmail.com>
2023-07-22 18:47:01 +0600

    single collage data api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000 ;
 const cors = require("cors")
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -34,6 +34,12 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/collage/:id', async(req, res)=>{
+      const id = req.params.id ;
+      const query = {_id : new ObjectId(id)}
+      const result = await collageCollection.findOne(query) ;
+      res.send(result)
+    })
 
 
 
