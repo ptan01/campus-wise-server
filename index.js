5b59953f292930066a895907ffd4d25bfbbc1c1b
ptan01 <ptanjim01@gmail.com>
2023-07-24 02:01:18 +0600

    my collage data api created

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/submitApplication/:email', async (req, res)=>{
+      const email = req.params.email ;
+      const query = {email : email} ;
+      const result = await submitAdmissionsCollection.find(query).toArray()
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
